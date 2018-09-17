18c413301ecf6b864124a6bb4462cf11ee614bf0
Odin <odinuv@gmail.com>
2018-09-17 12:40:55 +0200

    refactor: streamline code

--- tests/phpunit/TokenValidatorTest.php
+++ tests/phpunit/TokenValidatorTest.php
@@ -11,28 +11,28 @@ use PHPUnit\Framework\TestCase;
 
 class TokenValidatorTest extends TestCase
 {
-    public function testValidToken(): void
+    private function getClientMock(array $verifyTokenData): Client
     {
         $client = self::getMockBuilder(Client::class)
             ->disableOriginalConstructor()
             ->setMethods(['verifyToken'])
             ->getMock();
         $client->method('verifyToken')
-            ->willReturn(['bucketPermissions' => ['in.some-bucket' => 'read']]);
+            ->willReturn($verifyTokenData);
         /** @var Client $client */
+        return $client;
+    }
+
+    public function testValidToken(): void
+    {
+        $client = $this->getClientMock(['bucketPermissions' => ['in.some-bucket' => 'read']]);
         $validator = new TokenValidator($client);
         self::assertSame('in.some-bucket', $validator->validate());
     }
 
     public function testTokenWrongPermissions(): void
     {
-        $client = self::getMockBuilder(Client::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['verifyToken'])
-            ->getMock();
-        $client->method('verifyToken')
-            ->willReturn(['bucketPermissions' => ['in.some-bucket' => 'manage']]);
-        /** @var Client $client */
+        $client = $this->getClientMock(['bucketPermissions' => ['in.some-bucket' => 'manage']]);
         $validator = new TokenValidator($client);
         self::expectExceptionMessage('The token must have read-only permissions to the bucket "in.some-bucket".');
         self::expectException(UserException::class);
@@ -41,13 +41,18 @@ class TokenValidatorTest extends TestCase
 
     public function testTokenTooBroadPermissions(): void
     {
-        $client = self::getMockBuilder(Client::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['verifyToken'])
-            ->getMock();
-        $client->method('verifyToken')
-            ->willReturn(['bucketPermissions' => ['in.some-bucket' => 'read', 'out.another-bucket' => 'read']]);
-        /** @var Client $client */
+        $client = $this->getClientMock(
+            ['bucketPermissions' => ['in.some-bucket' => 'read', 'out.another-bucket' => 'read']]
+        );
+        $validator = new TokenValidator($client);
+        self::expectExceptionMessage('The token must have read-only permissions to a single bucket only.');
+        self::expectException(UserException::class);
+        $validator->validate();
+    }
+
+    public function testTokenTooNarrowPermissions(): void
+    {
+        $client = $this->getClientMock(['bucketPermissions' => []]);
         $validator = new TokenValidator($client);
         self::expectExceptionMessage('The token must have read-only permissions to a single bucket only.');
         self::expectException(UserException::class);
